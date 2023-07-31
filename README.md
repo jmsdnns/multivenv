bbea9f1baa454b230d1b761e6590d3c2574c51ca
Jms Dnns <jdennis@gmail.com>
2023-07-31 19:31:24 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MultiVenv
+
+I wanted something lighter than virtualenv wrapper that does basically the same thing. That's it.
+
+| Command | Description |
+| --- | --- |
+| mkvenv | make a new venv |
+| usevenv | enable a venv |
+| stopvenv | alias for deactivate |
+| rmvenv | delete a venv |
+| lsvenvs | list your venvs |
+| cdvenv | go to the directory root of a venv |
+
+Also, tab completion works for:
+* usevenv
+* rmvenv
+* cdvenv
---
+++ multivenv.sh
@@ -0,0 +1,120 @@
+# Multi Venv
+
+export MULTIVENV_HOME=$HOME/.multivenv
+export MULTIVENV_PYTHON="python3"
+
+function _multivenv_verify() {
+    if [[ -z $MULTIVENV_HOME ]]; then
+        mkdir $MULTIVENV_HOME
+    fi
+
+    export MULTIVENV_HOME=$(realpath $MULTIVENV_HOME)
+}
+
+function usevenv() {
+    _multivenv_verify
+
+    if [[ ! -d $MULTIVENV_HOME/$1 ]]; then
+        echo "ERROR: venv does not exist"
+        return 1
+    fi
+
+    source $MULTIVENV_HOME/$1/bin/activate
+}
+
+function stopvenv() {
+    _multivenv_verify
+
+    if [[ -z $VIRTUAL_ENV ]]; then
+        echo "ERROR: no active venv found"
+        return 1
+    fi
+
+    deactivate
+}
+
+function mkvenv() {
+    _multivenv_verify
+
+    if [[ -z $1 ]]; then
+        echo "ERROR: No venv name given"
+        return 1
+    fi
+
+    if [[ -d $MULTIVENV_HOME/$1 ]]; then
+        echo "ERROR: venv name already in use"
+        return 1
+    fi
+
+    $MULTIVENV_PYTHON -mvenv $MULTIVENV_HOME/$1
+    source $MULTIVENV_HOME/$1/bin/activate
+}
+
+function rmvenv() {
+    _multivenv_verify
+
+    if [[ -z $1 ]]; then
+        echo "ERROR: No venv name given"
+        return 1
+    fi
+
+    if [[ ! -d $MULTIVENV_HOME/$1 ]]; then
+        echo "ERROR: venv does not exist"
+        return 1
+    fi
+
+    if [[ $VIRTUAL_ENV == $MULTIVENV_HOME/$1 ]]; then
+        echo "ERROR: cannot remove activate venv"
+        return 1
+    fi
+
+    read -r -p "Are you sure? [y/N] " response
+    response=${response,,} # tolower
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+        declare PRIOR_DIR=$(pwd)
+        cd $MULTIVENV_HOME
+        rm -rf $1
+
+        if [[ -d $PRIOR_DIR ]]; then
+            cd $PRIOR_DIR
+        fi
+    fi
+}
+
+function lsvenvs() {
+    _multivenv_verify
+
+    for FILE in $(ls $MULTIVENV_HOME); do
+        echo $FILE
+    done
+}
+
+function cdvenv() {
+    _multivenv_verify
+
+    if [[ -z $1 ]]; then
+        echo "ERROR: No venv name given"
+        return 1
+    fi
+
+    if [[ ! -d $MULTIVENV_HOME/$1 ]]; then
+        echo "ERROR: venv does not exist"
+        return 1
+    fi
+
+    cd $MULTIVENV_HOME/$1
+}
+
+# tab completions
+
+function _listvenvs()
+{
+    local cur
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=($( compgen -W "$(ls $MULTIVENV_HOME)" -- $cur ) )
+}
+
+complete -F _listvenvs usevenv
+complete -F _listvenvs rmvenv
+complete -F _listvenvs cdvenv
